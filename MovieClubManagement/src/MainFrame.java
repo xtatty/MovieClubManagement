5480ead5825a087d5bda74d54826fce51f7cc9cb
xtatty <mai1514@uom.edu.gr>
2015-02-01 14:21:03 +0200

    setBounds to GUI

--- MovieClubManagement/src/MainFrame.java
+++ MovieClubManagement/src/MainFrame.java
@@ -392,7 +392,7 @@ public class MainFrame extends JFrame {
 		
 		this.setVisible(true);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 619, 475);
+		setBounds(400, 100, 619, 475);
 	}
 
 	/**
